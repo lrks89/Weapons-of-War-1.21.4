40f9aa2bb27a3858d9cacd770fb147ddc2045704
lrks89 <kendoka89@gmail.com>
2025-10-07 22:01:14 -0400

    Added Keybind and worked on animations

--- src/main/java/net/lrks89/wowmod/WeaponsOfWarClient.java
+++ src/main/java/net/lrks89/wowmod/WeaponsOfWarClient.java
@@ -1,10 +1,18 @@
 package net.lrks89.wowmod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.lrks89.wowmod.event.KeyInputHandler;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
 public class WeaponsOfWarClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
-
+        KeyInputHandler.register();
     }
 }
